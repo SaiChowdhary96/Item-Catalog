e46a4bb7651e27b6302dbe1150412a89f15e4232
SaiChowdhary96 <45555795+SaiChowdhary96@users.noreply.github.com>
2019-02-05 23:48:54 +0530

    removed extra white spaces

--- final_project.py
+++ final_project.py
@@ -394,6 +394,6 @@ def deleteGame(game_type_id, game_id):
 
 
 if __name__ == '__main__':
-        app.secret_key = 'super_secret_key'
-        app.debug = True
-        app.run(host="0.0.0.0", port=7000) 
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host="0.0.0.0", port=7000)
